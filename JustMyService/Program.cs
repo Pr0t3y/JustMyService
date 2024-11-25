4527d3802d9117d7ceb7d6d1c8d8aedb7ce4bcbc
Дудкин Иван <pr0t3y@yandex.ru>
2024-11-25 17:02:26 +0500

    DI

--- JustMyService/Program.cs
+++ JustMyService/Program.cs
@@ -1,6 +1,9 @@
+using JustMyService.Weather;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient<IWeatherService, OpenWeatherService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
