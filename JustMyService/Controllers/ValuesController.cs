bfa0fd918c226f9ee9d48a8b8e49162939bc02e3
Дудкин Иван <pr0t3y@yandex.ru>
2024-11-25 09:48:32 +0500

    Initial commit

---
+++ JustMyService/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Status = "Healthy", Timestamp = DateTime.UtcNow });
+        }
+    }
+}
