1cd82fc56faccfaef9f7fc7df3191402a4ad419f
Дудкин Иван <pr0t3y@yandex.ru>
2024-11-25 16:34:45 +0500

    Добавлены тесты

---
+++ JustMyService.Tests/HealthCheckControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using JustMyService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustMyService.Tests;
+public class HealthCheckControllerTests
+{
+    [Fact]
+    public void Get_ReturnsOkResult()
+    {
+        var controller = new HealthCheckController();
+        var result = controller.Get();
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(200, okResult.StatusCode);
+    }
+}
